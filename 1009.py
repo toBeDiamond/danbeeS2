a21cc00c0482cc0f79e2cf4ecb5e3cd5ff5b8aca
Danbi <silverwill39@gmail.com>
2023-07-25 21:38:51 +0900

    1009. 분산처리 수정

--- 1009.py
+++ 1009.py
@@ -1,59 +1,7 @@
 # 1009. 분산처리
-# for _ in range(int(input())):
-#     a, b = map(int, input().split())
-#     result = 1
-
-#     if b % 4 == 0:
-#         b = 4
-#     else:
-#         b = b % 4
-
-#     for i in range(1, b + 1):
-#         result *= a % 10
-#         result %= 10
-
-#         if result == 0:
-#             result = 10
-
-#     print(result)
-
-# print(0 % 4)
-
-for _ in range(int(input())):
-    a, b = map(int, input().split())
-
-    b %= 4
-    if b % 4 == 0:
-        b = 4
-
-    data = a**b
-    if data % 10 == 0:
-        print(10)
-    else:
-        print(data % 10)  # 1009. 분산처리
 import sys
 
 sys.stdin = open("input.txt")
-
-# for _ in range(int(input())):
-#     a, b = map(int, input().split())
-#     result = 1
-
-#     if b % 4 == 0:
-#         b = 4
-#     else:
-#         b = b % 4
-
-#     for i in range(1, b + 1):
-#         result *= a % 10
-#         result %= 10
-
-#         if result == 0:
-#             result = 10
-
-#     print(result)
-
-# print(0 % 4)
 
 for _ in range(int(input())):
     a, b = map(int, input().split())
@@ -67,3 +15,24 @@ for _ in range(int(input())):
         print(10)
     else:
         print(data % 10)
+
+
+# for _ in range(int(input())):
+#     a, b = map(int, input().split())
+#     result = 1
+
+#     if b % 4 == 0:
+#         b = 4
+#     else:
+#         b = b % 4
+
+#     for i in range(1, b + 1):
+#         result *= a % 10
+#         result %= 10
+
+#         if result == 0:
+#             result = 10
+
+#     print(result)
+
+# print(0 % 4)
